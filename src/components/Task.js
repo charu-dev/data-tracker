05eed943201470c5a5d9e8b71f951ec00de2ee83
charu-dev <charu@gupta.com>
2022-10-19 22:31:30 +0530

    green bar when reminder true

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {FaTimes} from 'react-icons/fa'
 function Task({task, onDelete, onToggle}) {
   return (
-    <div className='task' onDoubleClick={()=>{
+    <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={()=>{
         onToggle(task.id)
     }}><h3>{task.text} <FaTimes style={{color:'red', cursor:'pointer'}} onClick={()=>onDelete(task.id)} /> </h3></div>
   )
