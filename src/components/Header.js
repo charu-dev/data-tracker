a2ff59c02a67873a2c231ca80b433a1578b3c8c3
charu-dev <charu@gupta.com>
2022-10-29 00:32:27 +0530

    uselocation is used

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 const Header = ({title, onAdd, showAddTask}) => {
-    
+    const location =useLocation()
   return (
     <header className='header'><h1 style={headingStle}>Task tracker {title}</h1>
-    <Button text={showAddTask ?"Close":"Add"} bg={showAddTask ?"Red":"Green"} onClick={onAdd}/>
+    
+    {location.pathname==='/'&&( <Button text={showAddTask ?"Close":"Add"} bg={showAddTask ?"Red":"Green"} onClick={onAdd}/>
+   )}
     </header>
     
     )
